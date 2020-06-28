e1d1b5afc08ffe7c1dff00538972a1e50da8e639
caichenghao1991 <caichenghao1991@gmail.com>
2020-06-28 17:38:46 -0500

    v0

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO student(name, age) VALUES ('Harry Potter', 16);
 INSERT INTO student(name, age) VALUES ('Hermione Granger', 15);
 INSERT INTO student(name, age) VALUES ('Ron Weasley', 14);
+
 COMMIT;
